74b10e775a4db3a18ecff1c696c3ca6470b33eee
ranjeet3102 <ranjeetchandra3102@gmail.com>
2025-09-19 00:42:32 +0530

    Update frontend: fixed build issue and improved login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/auth/login/", {
+      const res = await fetch("https://ts-techy-crud-1.onrender.com/api/auth/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
